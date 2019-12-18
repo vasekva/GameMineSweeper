dbfa1c009bf24e76c5667b685319aeb4eabe8917
vasekva <vahonin_vasek@mail.ru>
2019-12-18 12:02:54 +0300

    Первая стадия с методами создания панели, окна, получением изображения на координату и заполнением ими всего окна из enum'a со значениями. *+ .jar файл запуска проги

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,10 +1,17 @@
+import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Game;
+import sweeper.Ranges;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+    private Game game;
+
     private JPanel panel;
-    private final int COLS = 15;
-    private final int ROWS = 1;
+    private final int COLS = 9;
+    private final int ROWS = 9;
     private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
@@ -13,6 +20,8 @@ public class JavaSweeper extends JFrame {
     }
 
     private JavaSweeper () {
+        game = new Game(COLS, ROWS);
+        setImage();
         initPanel();
         initFrame();
     }
@@ -22,10 +31,14 @@ public class JavaSweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(getImage("bomb"),0, 0, this );
+                for(Coord coord : Ranges.getAllCoords()) {
+                    g.drawImage((Image) game.getBox(coord).image, coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this );
+                }
             }
-        }; 
-        panel.setPreferredSize(new Dimension( COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+        };
+        panel.setPreferredSize(new Dimension(
+                Ranges.getSize().x * IMAGE_SIZE,
+                Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
 
@@ -36,7 +49,14 @@ public class JavaSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+        setIconImage(getImage("icon"));
     }
+    private void setImage() {
+        for(Box box : Box.values()) {
+            box.image = getImage(box.name().toLowerCase());
+        }
+    }
+
     private Image getImage(String name) {
         String filename = "img/" + name.toLowerCase() + ".png";
         ImageIcon icon = new ImageIcon(getClass().getResource(filename));
